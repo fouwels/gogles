a29fe002afaa89ed390f670701d288bae778fc57
Kaelan Fouwels <kaelan.fouwels@lagoni.co.uk>
2020-04-01 19:17:56 +0100

    add text(ure) man

--- renderman/renderman.go
+++ renderman/renderman.go
@@ -1,61 +1,39 @@
 package renderman
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/kaelanfouwels/gogles/fontman"
 	gl "github.com/kaelanfouwels/gogles/glow/gl"
+	"github.com/kaelanfouwels/gogles/textman"
 )
 
 const assetsDir string = "assets/"
 
 //RenderMan ..
 type RenderMan struct {
-	fontman   *fontman.Fontman
-	width     float32
-	height    float32
-	textCache map[string]Texture
+	textman *textman.Textman
+	fontman *fontman.Fontman
+	width   float32
+	height  float32
 }
 
 //NewRenderman ..
-func NewRenderman(width float32, height float32, fontman *fontman.Fontman) (*RenderMan, error) {
+func NewRenderman(width float32, height float32, textman *textman.Textman, fontman *fontman.Fontman) (*RenderMan, error) {
 
 	rm := RenderMan{
-		width:     width,
-		height:    height,
-		fontman:   fontman,
-		textCache: map[string]Texture{},
+		width:   width,
+		height:  height,
+		textman: textman,
+		fontman: fontman,
 	}
 
 	rm.initialize()
-
-	textures := []string{
-		"t_square",
-		"t_background",
-	}
-
-	for _, v := range textures {
-		f, err := os.Open(fmt.Sprintf("%v/%v.png", assetsDir, v))
-		if err != nil {
-			return nil, fmt.Errorf("Failed to open %v: %w", v, err)
-		}
-
-		text, err := loadTexture(f)
-		if err != nil {
-			return nil, fmt.Errorf("Failed to load %v: %w", v, err)
-		}
-		rm.textCache[v] = text
-	}
 
 	return &rm, nil
 }
 
 //Destroy ..
 func (r *RenderMan) Destroy() {
-	for _, v := range r.textCache {
-		gl.DeleteTextures(1, &v.ID)
-	}
+
 }
 
 func (r *RenderMan) initialize() {
@@ -75,33 +53,38 @@ func (r *RenderMan) initialize() {
 func (r *RenderMan) Draw() {
 
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	//r.drawBackground()
+	r.drawBackground()
 	r.drawForeground()
 	r.drawOverlay()
 }
 
 func (r *RenderMan) drawBackground() {
 
-	gl.Enable(gl.TEXTURE_2D)
-	gl.TexEnvf(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.REPLACE)
-	gl.BindTexture(gl.TEXTURE_2D, r.textCache["t_square"].ID)
+	// text, err := r.textman.GetText("t_square")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	gl.Begin(gl.QUADS)
-	gl.Color3f(1.0, 1.0, 1.0)
+	// gl.Enable(gl.TEXTURE_2D)
+	// gl.TexEnvf(gl.TEXTURE_ENV, gl.TEXTURE_ENV_MODE, gl.REPLACE)
+	// gl.BindTexture(gl.TEXTURE_2D, text.ID)
 
-	gl.TexCoord2f(0, 0)
-	gl.Vertex2f(-r.width/2, -r.height/2)
+	// gl.Begin(gl.QUADS)
+	// gl.Color3f(1.0, 1.0, 1.0)
 
-	gl.TexCoord2f(0, 1)
-	gl.Vertex2f(-r.width/2, r.height/2)
+	// gl.TexCoord2f(0, 0)
+	// gl.Vertex2f(-r.width/2, -r.height/2)
 
-	gl.TexCoord2f(1, 1)
-	gl.Vertex2f(r.width/2, r.height/2)
+	// gl.TexCoord2f(0, 1)
+	// gl.Vertex2f(-r.width/2, r.height/2)
 
-	gl.TexCoord2f(1, 0)
-	gl.Vertex2f(r.width/2, -r.height/2)
+	// gl.TexCoord2f(1, 1)
+	// gl.Vertex2f(r.width/2, r.height/2)
 
-	gl.End()
+	// gl.TexCoord2f(1, 0)
+	// gl.Vertex2f(r.width/2, -r.height/2)
+
+	// gl.End()
 }
 
 func (r *RenderMan) drawForeground() {
--- textman/textman.go
+++ textman/textman.go
@@ -1,4 +1,4 @@
-package renderman
+package textman
 
 import (
 	"fmt"
@@ -6,15 +6,70 @@ import (
 	"image/draw"
 	_ "image/png" // Load png decoder
 	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
 
 	gl "github.com/kaelanfouwels/gogles/glow/gl"
 )
+
+//Textman Texture Manager
+type Textman struct {
+	textCache map[string]Texture
+}
 
 //Texture ..
 type Texture struct {
 	Width  int
 	Height int
 	ID     uint32
+}
+
+//NewTextman ..
+func NewTextman(textFolder string) (*Textman, error) {
+
+	tman := Textman{
+		textCache: map[string]Texture{},
+	}
+
+	files, err := ioutil.ReadDir(textFolder)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to enumerate textFolder %v: %w", textFolder, err)
+	}
+
+	for _, f := range files {
+
+		file, err := os.Open(fmt.Sprintf("%v/%v", textFolder, f.Name()))
+		if err != nil {
+			return nil, fmt.Errorf("Failed to open texture %v: %w", f, err)
+		}
+		text, err := loadTexture(file)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to load texture %v: %w", f, err)
+		}
+
+		extension := filepath.Ext(f.Name())
+		tman.textCache[strings.TrimSuffix(f.Name(), extension)] = text
+	}
+
+	return &tman, nil
+}
+
+//GetText ...
+func (t *Textman) GetText(textName string) (Texture, error) {
+
+	if text, ok := t.textCache[textName]; ok {
+		return text, nil
+	}
+	return Texture{}, fmt.Errorf("Texture %s not present in texture cache", textName)
+}
+
+//Destroy ..
+func (t *Textman) Destroy() {
+	for _, v := range t.textCache {
+		gl.DeleteTextures(1, &v.ID)
+	}
 }
 
 func loadTexture(reader io.Reader) (Texture, error) {
