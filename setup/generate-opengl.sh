39d11e644a56fb02611ca01b5631069393036ba2
Kaelan Fouwels <kaelan.fouwels@lagoni.co.uk>
2020-04-22 18:34:41 +0100

    link to upstream glfw

--- setup/generate-opengl.sh
+++ setup/generate-opengl.sh
@@ -3,14 +3,14 @@
 GLOWDIR="$PWD/../glow"
 
 # Get and build GLFW
-go get -u -tags=GLFW_OSMESA github.com/go-gl/glfw/v3.3/glfw # OpenGL Mode
+go get -u github.com/go-gl/glfw/v3.3/glfw # OpenGL Mode
 #go get -u -tags=gles1 github.com/go-gl/glfw/v3.3/glfw # OpenGL ES Mode
 
 # Generate OpenGL bindings
 go get github.com/go-gl/glow
 go install github.com/go-gl/glow
 
-cd ./opengl && glow generate -api=gl -version=1.1 -out $GLOWDIR/gl # OpenGL Bindings
+cd ./opengl && glow generate -api=gl -version=2.1 -out $GLOWDIR/gl # OpenGL Bindings
 #glow generate -api=gles1 -version=1.0 -out $GLOWDIR/gles #  OpenGL ES Bindings
 cd ..
 
