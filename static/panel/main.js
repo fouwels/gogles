73464bd3e6131dd4fbf445b610c9bd5407025766
Kaelan Fouwels <kaelan.fouwels@lagoni.co.uk>
2020-04-24 14:11:02 +0100

    Add javascript

--- static/panel/main.js
+++ static/panel/main.js
@@ -2,6 +2,11 @@ var ticks = 0
 
 function render() {
     ticks++
+
+    $.getJSON("/api", function(data) {
+        $("#api_response").text(JSON.stringify(data, null, 2));
+    })
+
     document.getElementById("ticker").innerHTML = "Healthkeeper v0.1 | " + ticks
 }
 
